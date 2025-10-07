7f738dc40dee1029c8ed6ed70a0e23f6ef81fbee
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2025-10-07 20:43:19 +0200

    clean up

--- frontend/main.ts
+++ frontend/main.ts
@@ -7,43 +7,38 @@ const palette_colors = [
     "#42ff42",
     "#4242ff",
     "#aa42ff",
-]
-
-
+];
 
 class State {
-    selected_color: number
+    selected_color: number;
 
     constructor(selected_color: number = 1) {
-        this.selected_color = selected_color
+        this.selected_color = selected_color;
     }
 }
 
-let state = new State()
+let state = new State();
 
-let palette_e = document.querySelector(".palette")!
-if (!palette_e) {
-    throw new Error("No palette element")
-}
+let palette_e = document.querySelector("#palette")!;
 
 palette_colors.forEach((color, index) => {
-    let input_e = document.createElement("input")
-    input_e.type = "radio"
-    input_e.id = `color-${index}`
-    input_e.name = "color"
-    input_e.value = index.toString()
-    palette_e.appendChild(input_e)
+    let color_e = palette_e.appendChild(document.createElement("div"));
+    color_e.className = "color";
+
+    let input_e = color_e.appendChild(document.createElement("input"));
+    input_e.type = "radio";
+    input_e.id = `color-${index}`;
+    input_e.name = "color";
 
     if (index === state.selected_color) {
-        input_e.checked = true
+        input_e.checked = true;
     }
 
-    let label_e = document.createElement("label")
-    label_e.style.backgroundColor = color
-    label_e.htmlFor = input_e.id
-    palette_e.appendChild(label_e)
+    let label_e = color_e.appendChild(document.createElement("label"));
+    label_e.style.backgroundColor = color;
+    label_e.htmlFor = input_e.id;
 
-    input_e.addEventListener("change", (_) => {
-        state.selected_color = index
+    input_e.addEventListener("change", () => {
+        state.selected_color = index;
     })
 })
