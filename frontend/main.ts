eaf3c996ca09addc2a0bfda97ee54ccf22aecfa9
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2025-10-07 19:07:42 +0200

    palette generated with html

--- frontend/main.ts
+++ frontend/main.ts
@@ -1,7 +1,20 @@
-class State {
-    selected_color: number // Color index
+const palette_colors = [
+    "#FFFFFF",
+    "#000000",
+    "#ff4242",
+    "#ffaa00",
+    "#ffff42",
+    "#42ff42",
+    "#4242ff",
+    "#aa42ff",
+]
 
-    constructor(selected_color: number = 0) {
+
+
+class State {
+    selected_color: number
+
+    constructor(selected_color: number = 1) {
         this.selected_color = selected_color
     }
 }
@@ -13,14 +26,24 @@ if (!palette_e) {
     throw new Error("No palette element")
 }
 
-palette_e.querySelectorAll("input").forEach((e, i) => {
-    if (e.checked) {
-        console.log("Default color:", i)
-        state.selected_color = i
+palette_colors.forEach((color, index) => {
+    let input_e = document.createElement("input")
+    input_e.type = "radio"
+    input_e.id = `color-${index}`
+    input_e.name = "color"
+    input_e.value = index.toString()
+    palette_e.appendChild(input_e)
+
+    if (index === state.selected_color) {
+        input_e.checked = true
     }
 
-    e.addEventListener("click", () => {
-        console.log("Selected color:", i)
-        state.selected_color = i
+    let label_e = document.createElement("label")
+    label_e.style.backgroundColor = color
+    label_e.htmlFor = input_e.id
+    palette_e.appendChild(label_e)
+
+    input_e.addEventListener("change", (_) => {
+        state.selected_color = index
     })
 })
